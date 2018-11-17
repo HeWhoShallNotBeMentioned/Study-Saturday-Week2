181f37eeb7a4db311ecb839c0a0f6f501f9b60ae
Guest User <Guest@fullstacks-MacBook-Pro.local>
2018-11-17 11:49:19 -0600

    finished cycle 1

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,29 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: { isEmail: true}
+    }
+});
+
+Student.beforeSave((student) => {
+    student.firstName = capitalize(student.firstName),
+    student.lastName = capitalize(student.lastName);
+})
+
+const capitalize = (s) => {
+    return s.charAt(0).toUpperCase() + s.slice(1)
+  };
 
 module.exports = Student;
